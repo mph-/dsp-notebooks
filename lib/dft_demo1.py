afbd6677bbfbbc6213376cf66748168e9c5ad693
Michael Hayes <michael.hayes@canterbury.ac.nz>
2018-06-21 08:58:03 +1200

    Add Plotter

--- lib/dft_demo1.py
+++ lib/dft_demo1.py
@@ -1,9 +1,9 @@
 # M. P. Hayes UCECE
 import numpy as np
 from ipywidgets import interact, interactive, fixed, interact
-from .signal_plot import signal_plot_with_dft
+from .signal_plot import signal_plot_with_dft, spectrum_modes
 
-def dft_demo1_plot(N=100, cycles=2, lollipop=True):
+def dft_demo1_plot(N=100, cycles=2, mode='real-imag', lollipop=True):
 
     fs = 100
     T = N / fs
@@ -15,10 +15,11 @@ def dft_demo1_plot(N=100, cycles=2, lollipop=True):
 
     f = np.arange(len(X)) / N * fs
 
-    signal_plot_with_dft(t, x, f, X, lollipop=lollipop)
+    signal_plot_with_dft(t, x, f, X, lollipop=lollipop, mode=mode)
 
 def dft_demo1():
     interact(dft_demo1_plot, N=(64, 512), cycles=(0, 50, 1),
+             mode=spectrum_modes,
              continuous_update=False)
     
     
--- lib/signal_plot.py
+++ lib/signal_plot.py
@@ -2,6 +2,8 @@
 from matplotlib.pyplot import subplots, setp
 import numpy as np
 
+spectrum_modes = ['real-imag', 'magnitude', 'magnitude dB', 'phase', 'magnitude-phase', 'magnitude dB-phase']
+
 def lollipop_plot(n, x, axes=None, markersize=6, **kwargs):
     """Produce a lollipop (stem) plot."""
 
@@ -13,6 +15,89 @@ def lollipop_plot(n, x, axes=None, markersize=6, **kwargs):
     setp(markerline, 'markersize', markersize, 'color', color)
     setp(stemlines, 'color', color)    
 
+
+class Plotter(object):
+
+    def plot_lines(self, x, y, **kwargs):
+        axes = kwargs.pop('axes', self.axes)
+        axes.plot(x, y, **kwargs)
+        axes.set_xlabel(self.xlabel)
+
+    def plot_lollipop(self, x, y, **kwargs):
+        axes = kwargs.pop('axes', self.axes)        
+        lollipop_plot(x, y, axes=axes, **kwargs)
+        axes.set_xlabel(self.xlabel)        
+
+    def __init__(self, axes, mode='time', lollipop=True):
+        self.axes = axes
+        self.mode = mode
+        self.lollipop = lollipop
+        self.plot_method = self.plot_lollipop if lollipop else self.plot_lines
+        if mode == 'time':
+            xlabel = 'Time'
+            if lollipop:
+                xlabel += ' (samples)'
+            else:
+                xlabel += ' (s)'
+        else:
+            xlabel = 'Frequency'
+            if lollipop:
+                xlabel += ' (samples)'
+            else:
+                xlabel += ' (Hz)'
+        self.xlabel = xlabel
+                
+
+    def plot(self, x, y, **kwargs):
+        if self.mode == 'time':
+            self.plot_method(x, y, **kwargs)
+            xlabel = 'Time (s)'
+            if self.lollipop:
+                xlabel = 'Time (samples)'                
+            self.axes.set_xlabel(xlabel)
+
+        elif self.mode == 'real-imag':
+            self.plot_method(x, y.real, label='real', **kwargs)
+            self.plot_method(x, y.imag, label='imag', color='orange', **kwargs)
+            self.axes.legend()
+            
+        elif self.mode == 'magnitude':
+            self.plot_method(x, abs(y), label='magnitude', **kwargs)
+            self.axes.set_ylabel('Magnitude')
+
+        elif self.mode == 'magnitude dB':
+            dB = 20 * np.log10(abs(y))
+            dB[dB < -100] = -100
+            self.plot_method(x, dB, label='magnitude', **kwargs)
+            self.axes.set_ylabel('Magnitude (dB)')
+
+        elif self.mode == 'phase':
+            self.plot_method(x, np.degrees(np.arctan2(y.imag, y.real)), label='phase', color='orange', **kwargs)
+            self.axes.set_ylabel('Phase (deg)')
+
+        elif self.mode == 'magnitude-phase':
+            axes2 = self.axes.twinx()
+            self.plot_method(x, abs(y), label='magnitude', **kwargs)
+            self.axes.set_ylabel('Magnitude')
+            self.plot_method(x, np.degrees(np.arctan2(y.imag, y.real)), label='phase', axes=axes2, color='orange', **kwargs)
+            axes2.set_ylabel('Phase (deg)')
+            self.axes.legend()
+            axes2.legend()            
+            
+        elif self.mode == 'magnitude dB-phase':
+            axes2 = self.axes.twinx()
+            dB = 20 * np.log10(abs(y))
+            dB[dB < -100] = -100            
+            self.plot_method(x, dB, label='magnitude', **kwargs)
+            self.axes.set_ylabel('Magnitude (dB)')
+            self.plot_method(x, np.degrees(np.arctan2(y.imag, y.real)), label='phase', axes=axes2, color='orange', **kwargs)
+            axes2.set_ylabel('Phase (deg)')
+            self.axes.legend()
+            axes2.legend()            
+            
+        else:
+            raise ValueError('Unknown mode %s' % mode)
+        
 def one_axes(**kwargs):
 
     axes = kwargs.pop('axes', None)
@@ -29,6 +114,7 @@ def two_axes(**kwargs):
 
     if axes is None:    
         fig, axes = subplots(2, 1, figsize=figsize)
+        fig.subplots_adjust(hspace=0.4)
     return axes, kwargs
 
 def signal_plot_discrete(t, x, axes, **kwargs):
@@ -90,13 +176,11 @@ def signal_plot_with_hist(t, x, **kwargs):
 
     return axes[0].figure    
 
-def dtft_plot(f, X, **kwargs):
+def dtft_plot(f, X, mode='real-imag', **kwargs):
 
     axes, kwargs = one_axes(**kwargs)
-    axes.plot(f, X.real, label='real', **kwargs)
-    axes.plot(f, X.imag, label='imag', **kwargs)    
-    axes.set_xlabel('Frequency (Hz)')
-    axes.legend()    
+
+    Plotter(axes, mode, lollipop=False).plot(f, X)
     
 def signal_plot_with_dtft(t, x, f, X, **kwargs):
 
@@ -109,26 +193,19 @@ def signal_plot_with_dtft(t, x, f, X, **kwargs):
         
     return axes[0].figure
 
-def dft_plot(f, X, lollipop=False, **kwargs):
+def dft_plot(f, X, lollipop=False, mode='real-imag', **kwargs):
     axes, kwargs = one_axes(**kwargs)
 
-    if lollipop:
-        lollipop_plot(f, X.real, label='real', axes=axes, **kwargs)
-        lollipop_plot(f, X.imag, label='imag', axes=axes, **kwargs, color='orange')
-        axes.set_xlabel('Sample')
-    else:
-        axes.plot(f, X.real, label='real', **kwargs)
-        axes.plot(f, X.imag, label='imag', **kwargs)    
-        axes.set_xlabel('Frequency (Hz)')
-    axes.legend()
+    Plotter(axes, mode, lollipop).plot(f, X)
 
 def signal_plot_with_dft(t, x, f, X, **kwargs):
 
     axes, kwargs = two_axes(**kwargs)
 
     lollipop = kwargs.pop('lollipop', False)
+    mode = kwargs.pop('mode', 'real-imag')    
     
     signal_plot(t, x, axes=axes[0], lollipop=lollipop, **kwargs)
-    dft_plot(f, X, axes=axes[1], lollipop=lollipop, **kwargs)    
+    dft_plot(f, X, axes=axes[1], lollipop=lollipop, mode=mode, **kwargs)    
         
     return axes[0].figure
