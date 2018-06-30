37acf130ef88265eadb3d437758200bc368d1684
Michael Hayes <michael.hayes@canterbury.ac.nz>
2018-06-30 14:14:10 +1200

    Add gated cw

--- intro/lib/fourier_demo1.py
+++ intro/lib/fourier_demo1.py
@@ -40,7 +40,24 @@ def sinc_demo1():
     interact(sinc_demo1_plot, T=(0.1, 5, 0.1), mode=spectrum_modes,
              continuous_update=False)
     
-    
-    
-    
+def gated_cw_demo1_plot(T=1, f0=5, phase=0, mode='real-imag'):
 
+    tmax = 5
+    fmax = 20
+    N = 1000
+
+    t = np.linspace(-tmax, tmax, N)
+    f = np.linspace(-fmax, fmax, N)    
+
+    x = rect(t / T) * np.cos(2 * np.pi * f0 * t + np.radians(phase))
+
+    X = 0.5 * T * sinc((f - f0) * T) * np.exp(-1j * np.radians(phase)) + 0.5 * T * sinc((f + f0) * T) * np.exp(1j * np.radians(phase))
+    
+    signal_plot_with_dft(t, x, f, X, mode=mode)
+
+def gated_cw_demo1():
+    interact(gated_cw_demo1_plot, T=(0.1, 8, 0.1),
+             f0=(0, 10),
+             phase=(-180, 180, 15),
+             mode=spectrum_modes,
+             continuous_update=False)
