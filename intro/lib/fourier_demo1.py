49d1b74a26eb6e5526c4f0161a287fc7a8161bcd
Michael Hayes <michael.hayes@canterbury.ac.nz>
2018-06-30 17:53:07 +1200

    Add time shift

--- intro/lib/fourier_demo1.py
+++ intro/lib/fourier_demo1.py
@@ -41,7 +41,7 @@ def sinc_demo1():
              continuous_update=False)
     
 
-def gated_cw_demo1_plot(T=1, f0=5, phase=0, mode='real-imag'):
+def modulated_pulse_demo1_plot(T=1, f0=5, phase=0, mode='real-imag'):
 
     tmax = 5
     fmax = 20
@@ -56,8 +56,8 @@ def gated_cw_demo1_plot(T=1, f0=5, phase=0, mode='real-imag'):
     
     signal_plot_with_dft(t, x, f, X, mode=mode)
 
-def gated_cw_demo1():
-    interact(gated_cw_demo1_plot, T=(0.1, 8, 0.1),
+def modulated_pulse_demo1():
+    interact(modulated_pulse_demo1_plot, T=(0.1, 8, 0.1),
              f0=(0, 10),
              phase=(-180, 180, 15),
              mode=spectrum_modes,
@@ -81,6 +81,32 @@ def dirac_delta_demo1_plot(alpha=0.05, mode='real-imag'):
 
 def dirac_delta_demo1():
     interact(dirac_delta_demo1_plot, alpha=(0.00001, 0.1, 0.001),
+             mode=spectrum_modes,
+             continuous_update=False)
+    
+
+def time_shift_demo1_plot(delay=0, mode='real-imag'):
+
+    tmax = 5
+    fmax = 5
+    N = 1000
+
+    T = 4
+    f0 = 2
+    phase = 0
+
+    t = np.linspace(-tmax, tmax, N)
+    f = np.linspace(-fmax, fmax, N)    
+
+    t1 = t - delay
+    x = rect(t1 / T) * np.cos(2 * np.pi * f0 * t1 + np.radians(phase))
+
+    X = (0.5 * T * sinc((f - f0) * T) * np.exp(-1j * np.radians(phase)) + 0.5 * T * sinc((f + f0) * T) * np.exp(1j * np.radians(phase))) * np.exp(-1j * 2 * np.pi * f * delay)
+    
+    signal_plot_with_dft(t, x, f, X, mode=mode)
+
+def time_shift_demo1():
+    interact(time_shift_demo1_plot, delay=(-2, 2, 0.1),
              mode=spectrum_modes,
              continuous_update=False)
     
