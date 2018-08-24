31b73749d03276cc5d71f9027e66c16c66f874e9
Michael Hayes <michael.hayes@canterbury.ac.nz>
2018-08-25 11:12:12 +1200

    Display seq as sum of impulses

--- intro/demos/plotseq_demo1.py
+++ intro/demos/plotseq_demo1.py
@@ -2,7 +2,41 @@
 import numpy as np
 import scipy.signal as signal
 from ipywidgets import interact, interactive, fixed, interact
+from IPython.display import display, Math
 from .lib.signal_plot import signal_plot
+
+def foo(join, val, index):
+
+    num = ''
+    sign = '+'
+    if val < 0:
+        sign = '-'
+        val = -val
+    if val.is_integer:
+        if val == 1:
+            num = ''
+        else:
+            num = '%d' % val
+    else:
+        num = '%s' % val        
+        
+    if index == 0:
+        term = r'%s \delta[n]' % num
+    elif index < 0:
+        term = r'%s \delta[n + %d]' % (num, -index)
+    else:
+        term = r'%s \delta[n - %d]' % (num, index)        
+
+    if not join:
+        return term
+    return sign + ' ' + term
+
+def sumimpulses(seq, offset=0):
+
+    string = 'x[n] = '
+    for m, val in enumerate(seq):
+        string += foo(m != 0, val, m - offset)
+    return string
 
 def plotseq_demo1_plot(sequence='{1, _2, 3, 4, 5}'):
 
@@ -34,7 +68,8 @@ def plotseq_demo1_plot(sequence='{1, _2, 3, 4, 5}'):
         
     fig = signal_plot(t - underscore, seq, lollipop=True)
     fig.axes[0].grid(True)
-    
 
+    display(Math(sumimpulses(seq, underscore)))
+    
 def plotseq_demo1():
     interact(plotseq_demo1_plot, continuous_update=False)             
