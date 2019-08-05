0abbad2e311bbccbddd9854a107a0de7d062d88c
Michael Hayes <michael.hayes@canterbury.ac.nz>
2019-08-05 13:43:16 +1200

    Tweak

--- intro/demos/__init__.py
+++ intro/demos/__init__.py
@@ -78,3 +78,5 @@ from .polezero_demo4 import *
 from .polezero_gen1_demo1 import *
 from .polezero_gen2_demo1 import *
 from .polezero_gen4_demo1 import *
+from .mystery_audio_demo1 import *
+from .mystery_audio_demo2 import *
