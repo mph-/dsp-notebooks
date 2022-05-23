c3c65f6e71517461f7e27bc7a849e7ce614ecd6b
Michael Hayes <michael.hayes@canterbury.ac.nz>
2022-05-24 08:36:20 +1200

    Rename remez to PM

--- intro/demos/parks_mcclellan_demo1.py
+++ intro/demos/parks_mcclellan_demo1.py
@@ -6,11 +6,11 @@ from .lib.dt_generic import DTGeneric, response_modes
 from IPython.display import display, Math, Latex
 
 
-def remez_demo1_plot(numtaps=20, cutoff=100, trans_width=20, fs=400,
-                     mode=response_modes[2]):
+def parks_mcclellan_demo1_plot(order=20, cutoff=100, trans_width=20, fs=400,
+                               mode=response_modes[2]):
 
-    b = remez(numtaps, [0, cutoff, cutoff + trans_width,
-                        0.5 * fs], [1, 0], fs=fs)
+    b = remez(order, [0, cutoff, cutoff + trans_width,
+                      0.5 * fs], [1, 0], fs=fs)
     dtfilter = DTGeneric(b, [1], fs=fs)
 
     dtfilter.polezero_plot_with_response(50, mode=mode)
@@ -25,9 +25,9 @@ def remez_demo1_plot(numtaps=20, cutoff=100, trans_width=20, fs=400,
         print('a = %s, b = %s' % (fmt(a), fmt(b)))
 
 
-def remez_demo1():
-    interact(remez_demo1_plot,
-             numtaps=(5, 100, 5), cutoff=(10, 200, 10),
+def parks_mcclellan_demo1():
+    interact(parks_mcclellan_demo1_plot,
+             order=(5, 100, 5), cutoff=(10, 200, 10),
              trans_width=(10, 100, 10),
              fs=(200, 500, 50),
              mode=response_modes, continuous_update=False)
